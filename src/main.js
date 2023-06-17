2c8cc87972760e146a4e7df1de5ae56f2391e5b7
yangyanjun <375663014@qq.com>
2023-06-17 17:24:35 +0800

    spa-child-vue_first_commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,40 @@
-import Vue from 'vue'
+import Vue, { h } from 'vue'
 import App from './App.vue'
 import router from './router'
+import singleSpaVue from 'single-spa-vue';
 
 Vue.config.productionTip = false
 
-new Vue({
+// new Vue({
+//   router,
+//   render: h => h(App)
+// }).$mount('#app')
+const appOptions = {
+  el: '#vue', // 挂载到父应用中的id为vue的标签中
   router,
   render: h => h(App)
-}).$mount('#app')
+}
+// 我们需要父应用加载子应用
+// bootstrap  mount unmount 
+// single-spa / single-spa-vue 可以协助生成以上三个生命周期
+const vueLifeCycle = singleSpaVue({
+  Vue,
+  appOptions,
+});
+// 如果是父应用引用我
+if(window.singleSpaNavigate) {
+  __webpack_public_path__ = 'http://localhost:10000/'
+}
+// 单独启用
+if(!window.singleSpaNavigate) {
+  delete appOptions.el;
+  new Vue(appOptions).$mount('#app');
+}
+
+
+// 协议接入 我定好了协议 父应用会调用这些方法
+export const bootstrap = vueLifeCycle.bootstrap;
+export const mount = vueLifeCycle.mount;
+export const unmount = vueLifeCycle.unmount;
+
+// 我们需要父应用加载子应用，所以需要将子应用打包成一个个的lib去给父应用使用
